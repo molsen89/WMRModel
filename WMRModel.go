16d3916b0ecae11f2b57145dc7889fb614d5cb95
molsen89 <166087019+molsen89@users.noreply.github.com>
2025-09-28 20:35:47 -0500

    Added comments to each section for readability

--- WMRModel.go
+++ WMRModel.go
@@ -7,9 +7,16 @@ import (
 )
 
 func main() {
+
+	// Start timer
 	start := time.Now()
+	// Define variables
 	var width, length, height float64
 	var WMR_CaRoomt0, WMR_CaRoomCaIn, WMR_CaRoomGa, WMR_CaRoomQft3, WMR_proct, WMR_shiftt, WMR_OEL float64
+
+	// User inputs for Well-Mixed Room Model (WMR), room dimensions, and shift parameters
+	// All units are in feet, minutes, and mg/m3
+	// Ventilation rate is in ft3/min
 	fmt.Print("Enter width: (ft)")
 	fmt.Scan(&width)
 	fmt.Print("Enter length: (ft)")
@@ -23,7 +30,6 @@ func main() {
 	fmt.Print("Enter the contaminant generation rate: (mg/min)")
 	fmt.Scan(&WMR_CaRoomGa)
 	fmt.Print("Enter the ventilation rate (ft3/min): ")
-
 	fmt.Scan(&WMR_CaRoomQft3)
 	fmt.Print("Enter the process duration (min): ")
 	fmt.Scan(&WMR_proct)
@@ -31,23 +37,32 @@ func main() {
 	fmt.Scan(&WMR_shiftt)
 	fmt.Print("Enter the occupational exposure limit (mg/m3): ")
 	fmt.Scan(&WMR_OEL)
+
+	// Calculations for room dimensions, steady-state concentration, and time-weighted average concentration
 	dimensionsft3 := width * length * height
 	dimensionsm3 := dimensionsft3 * 0.0283168
 	WMR_CaRoomQm3 := WMR_CaRoomQft3 * 0.0283168
 	WMR_SteadyState_ProcT := (WMR_CaRoomt0-WMR_CaRoomCaIn-(WMR_CaRoomGa/WMR_CaRoomQm3))*(math.Exp((-WMR_CaRoomQm3*WMR_proct)/dimensionsm3)) + WMR_CaRoomCaIn + (WMR_CaRoomGa / WMR_CaRoomQm3)
 	TWA := (WMR_SteadyState_ProcT * WMR_proct) / WMR_shiftt
 
+	// Output results and check against occupational exposure limit
 	fmt.Printf("The steady-state concentration at the end of the process is: %.2f mg/m3\n", WMR_SteadyState_ProcT)
+
+	// Check if steady-state concentration exceeds OEL
 	if WMR_SteadyState_ProcT > WMR_OEL {
 		fmt.Println("The concentration exceeds the occupational exposure limit.")
 	} else {
 		fmt.Println("The concentration is within the occupational exposure limit.")
 	}
 	fmt.Printf("The time-weighted average concentration over the shift is: %.2f mg/m3\n", TWA)
+
+	// Check if TWA exceeds OEL
 	if TWA > WMR_OEL {
 		fmt.Println("The time-weighted average concentration exceeds the occupational exposure limit.")
 	} else {
 		fmt.Println("The time-weighted average concentration is within the occupational exposure limit.")
 	}
+
+	// Print execution time
 	fmt.Printf("Execution Time: %s\n", time.Since(start))
 }
